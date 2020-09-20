7e5e01380390b8c244f664a6c58aeaa70552d2e4
Chance Nguyen <chanceknguyen@gmail.com>
2020-09-19 19:22:39 -0500

    Implement functionality for receiving form data and change json to csv data

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,15 +1,4 @@
-// const heading = document.createElement("h1");
-// const heading_text = document.createTextNode("Big Head!");
-// heading.appendChild(heading_text);
-// document.body.appendChild(heading);
-
 var board = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-];
-
-var newBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
@@ -17,54 +6,78 @@ var newBoard = [
 
 var nextX = true;
 var winner;
+var endGame;
 var gameOver = false;
 var placedCounter = 0;
+var xCounter = 0;
+var oCounter = 0;
+
+var displayWins = function() {
+  document.getElementById('x-wins').innerHTML = xCounter;
+  document.getElementById('o-wins').innerHTML = oCounter;
+}
+
+var displayTurn = function() {
+  if(nextX) {
+    document.getElementById('turn').innerHTML = "It is X's turn!";
+  } else {
+    document.getElementById('turn').innerHTML = "It is O's turn!";
+  }
+}
 
 var checkWin = function() {
   if (board[0][0] !== null){
     if (board[0][0] === board[0][1] && board[0][0] === board[0][2]) {
-      winner = `Game is over! ${board[0][0]} has won. Please start a new game!`;
+      endGame = `Game is over! ${board[0][0]} has won. Please start a new game!`;
+      winner = board[0][0];
       return true;
     }
     if (board[0][0] === board[1][0] && board[0][0] === board[2][0]) {
-      winner = `Game is over! ${board[0][0]} has won. Please start a new game!`;
+      endGame = `Game is over! ${board[0][0]} has won. Please start a new game!`;
+      winner = board[0][0];
       return true;
     }
     if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
-      winner = `Game is over! ${board[0][0]} has won. Please start a new game!`;
+      endGame = `Game is over! ${board[0][0]} has won. Please start a new game!`;
+      winner = board[0][0];
       return true;
     }
   }
   if (board[0][1] !== null){
     if (board[0][1] === board[1][1] && board[0][1] === board[2][1]) {
-      winner = `Game is over! ${board[0][1]} has won. Please start a new game!`;
+      endGame = `Game is over! ${board[0][1]} has won. Please start a new game!`;
+      winner = board[0][1];
       return true;
     }
   }
   if (board[0][2] !== null){
     if (board[0][2] === board[1][2] && board[0][2] === board[2][2]) {
-      winner = `Game is over! ${board[0][2]} has won. Please start a new game!`;
+      endGame = `Game is over! ${board[0][2]} has won. Please start a new game!`;
+      winner = board[0][2];
       return true;
     }
   }
   if (board[1][0] !== null){
     if (board[1][0] === board[1][1] && board[1][0] === board[1][2]) {
-      winner = `Game is over! ${board[1][0]} has won. Please start a new game!`;
+      endGame = `Game is over! ${board[1][0]} has won. Please start a new game!`;
+      winner = board[1][0];
       return true;
     }
   }
   if (board[2][0] !== null){
     if (board[2][0] === board[2][1] && board[2][0] === board[2][2]) {
-      winner = `Game is over! ${board[2][0]} has won. Please start a new game!`;
+      endGame = `Game is over! ${board[2][0]} has won. Please start a new game!`;
+      winner = board[2][0];
       return true;
     }
     if (board[2][0] === board[1][1] && board[2][0] === board[0][2]) {
-      winner = `Game is over! ${board[2][0]} has won. Please start a new game!`;
+      endGame = `Game is over! ${board[2][0]} has won. Please start a new game!`;
+      winner = board[2][0];
       return true;
     }
   }
   if (placedCounter === 9) {
-    winner = 'Tie Game!';
+    endGame = 'Tie Game!';
     return true;
   }
   return false;
@@ -78,35 +91,24 @@ var resetBoard = function() {
     [null, null, null]
   ];
   setUpBoard(board);
-  alert('New game is ready!');
+  alert('New game is ready! Winner goes first!');
 }
 
-var boardPosition = function(number) {
-  if (number === '0' ) {
-    return {row: 0, column: 0};
-  } else if (number === '1' ) {
-    return {row: 0, column: 1};
-  } else if (number === '2' ) {
-    return {row: 0, column: 2};
-  } else if (number === '3' ) {
-    return {row: 1, column: 0};
-  } else if (number === '4' ) {
-    return {row: 1, column: 1};
-  } else if (number === '5' ) {
-    return {row: 1, column: 2};
-  } else if (number === '6') {
-    return {row: 2, column: 0};
-  } else if (number === '7' ) {
-    return {row: 2, column: 1};
-  } else if (number === '8' ) {
-    return {row: 2, column: 2};
-  };
-
-};
+var boardPosition = {
+  '0': {row: 0, column: 0},
+  '1': {row: 0, column: 1},
+  '2': {row: 0, column: 2},
+  '3': {row: 1, column: 0},
+  '4': {row: 1, column: 1},
+  '5': {row: 1, column: 2},
+  '6': {row: 2, column: 0},
+  '7': {row: 2, column: 1},
+  '8': {row: 2, column: 2},
+}
 
 var placeTile = function(number) {
   if(!gameOver) {
-    var coordinates = boardPosition(number);
+    var coordinates = boardPosition[number];
     if (board[coordinates.row][coordinates.column] === null) {
       if (nextX) {
         board[coordinates.row][coordinates.column] = 'X';
@@ -115,12 +117,19 @@ var placeTile = function(number) {
       };
       document.getElementById(number).innerHTML = board[coordinates.row][coordinates.column];
       nextX = !nextX;
+      displayTurn();
     } else {
       alert("Move cannot be made here! Please pick another box!")
     }
     if(checkWin()) {
       gameOver = true;
-      alert(`${winner}`);
+      if(winner === 'X') {
+        xCounter++;
+      } else {
+        oCounter++;
+      }
+      displayWins();
+      alert(`${endGame}`);
     }
   } else {
     alert(`Please start a new game!`)
@@ -128,6 +137,7 @@ var placeTile = function(number) {
 }
 
 var boardArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+
 
 var setUpBoard = function(gameBoard) {
 
@@ -143,6 +153,7 @@ var setUpBoard = function(gameBoard) {
 }
 
 setUpBoard(board);
+displayWins();
 
 document.getElementById("app").addEventListener('click', function(event) {
   placedCounter++;
@@ -152,12 +163,19 @@ document.getElementById("app").addEventListener('click', function(event) {
 document.getElementById("reset").addEventListener('click', function(event) {
   placedCounter = 0;
   gameOver = !gameOver;
-  nextX = true;
+  if(winner === 'X') {
+    nextX = true;
+  } else {
+    nextX = false;
+  }
+
+  document.getElementById('x-wins').addEventListener('click', function(event) {
+    (console.log('clicked'))
+  });
   resetBoard();
+  displayTurn();
 })
-// document.addEventListener('click', function(event) {
-//   console.log(event.target.id);
-// })
+
 
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,67 @@
+const express = require('express');
+const path = require('path');
+const port = 3000;
+const app = express();
+const fs = require('fs');
+const multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploaded/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+const upload = multer({storage: storage})
+// const bodyParser = require('body-parser');
 
+var jsonToCSV = function(jsonData) {
+  var results = [];
+  var overallKeys = Object.keys(jsonData);
+  overallKeys.pop();
+  var storage = {};
+  var recursion = function(data) {
+      var first = overallKeys[0];
+      var firstPoint = data[first];
+      storage[firstPoint] = [];
+      for (var i = 0; i < overallKeys.length; i++) {
+        var currentKey = overallKeys[i];
+        storage[firstPoint].push(data[currentKey]);
+      }
+      if (data.children.length > 1) {
+          for (var j = 0; j < data.children.length; j++) {
+              recursion(data.children[j]);
+          }
+      }
+  }
 
+  recursion(jsonData);
+for (var prop in storage) {
+    var currentString = storage[prop].join(',');
+    results.push(currentString);
+}
+var finalCSV = results.join('\n');
+fs.writeFile('finalCSV.csv', finalCSV, (err) => {
+  if (err) throw err;
+  console.log('file has been saved!')
+} )
+}
+
+app.use('/', express.static(path.join(__dirname,'client')));
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.post('/api/csv', upload.single('jsonFile'), (req, res) => {
+  // debugger;
+  console.log(req.file.originalname);
+
+  // fs.readFile(`./uploaded/${req.file.originalname}`, (err, data) => {
+  //   if (err) throw err;
+  //   console.log(data);
+
+  // })
+})
+
+app.listen(port, () => {
+  console.log(`CSV app listening at http://localhost:${port}`);
+})
